0b3ba9eb5975467519e97b2b477633afc932e08e
Wojtek <wojwat@o2.pl>
2019-10-19 10:03:38 +0200

    ignore

---
+++ src/week5/Pet.java
@@ -0,0 +1,22 @@
+package week5;
+
+public class Pet {
+    String name;
+    String breed;
+
+
+    public void sleep()
+    {
+        System.out.println("Im going to sleep now ");
+    }
+
+    public void eat()
+    {
+        System.out.println("Im going to eat now");
+    }
+
+    public void makeSound()
+    {
+        System.out.println("Agrrr");
+    }
+}
---
+++ src/week5/PetTester.java
@@ -0,0 +1,20 @@
+package week5;
+
+public class PetTester {
+    public static void main(String[] args) {
+        Cat tom = new Cat();
+        tom.name = "Tom";
+        tom.breed = "British";
+        tom.eat();
+        tom.sleep();
+        tom.makeSound();
+
+        Dog hunter = new Dog();
+        hunter.name = "Tom";
+        hunter.breed = "British";
+        hunter.eat();
+        hunter.sleep();
+        hunter.makeSound();
+
+    }
+}
